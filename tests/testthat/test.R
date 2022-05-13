3dbdec7b12f5cbed92d02871a566f16d7dc55515
Tomomi Sugiyama <sugiyama.t.am@m.titech.ac.jp>
2022-05-13 04:40:05 +0000

    version 0.1.1

---
+++ tests/testthat/test.R
@@ -0,0 +1,18 @@
+# Test
+## Prepare tables
+microbiome_table <- read.csv("../input_data/India_LOC1_genus.csv")
+metabolome_table <- read.csv("../input_data/India_LOC1_serum_metabolome.csv")
+
+## Filtering functions
+list_n <- filter_n(a_mat = microbiome_table, b_mat = metabolome_table,
+                           a_category = "genus", b_category = "metabolome",
+                           min_cor = 0.3, min_sample = 5, min_r2 = 0.1)
+list_40 <- filter_40(a_mat = microbiome_table, b_mat = metabolome_table,
+                             a_category = "genus", b_category = "metabolome",
+                             min_cor = 0.3, min_r2 = 0.1)
+test_that("normal filtering has failed", {
+  expect_equal(nrow(list_n), 37)
+})
+test_that("40% filtering has failed", {
+  expect_equal(nrow(list_40), 6)
+})
